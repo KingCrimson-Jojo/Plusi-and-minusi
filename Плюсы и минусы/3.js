54d30bdf12d5b419e266ede76cdb7afdbdafe7fa
Full_Hunter <113694125+KingCrimson-Jojo@users.noreply.github.com>
2023-06-02 20:33:43 +0300

    Add files via upload

---
+++ Плюсы и минусы/3.js
@@ -0,0 +1,95 @@
+let playerSymbol = "+";
+let gameEnded = false
+for (let i = 1; i <= 9; i++) {
+    
+    document.getElementById(i.toString()).addEventListener(
+    "click", 
+    function() {
+    if (this.innerHTML === "" && !gameEnded) {
+    
+    this.innerHTML = playerSymbol;
+    this.classList.add(playerSymbol.toLowerCase());
+    
+    
+    
+    checkWin();
+    
+          
+            if (playerSymbol === "+")
+            playerSymbol = "-"
+            else
+            playerSymbol = "+"
+            
+            
+            }
+        }
+            
+     );
+} 
+
+
+
+for (let i = 1; i <= 9; i++) {
+    
+    document.getElementById(i.toString()).addEventListener(
+        "click", 
+         function() {
+        if (this.innerHTML === "" && !gameEnded) {
+        if (playerSymbol === "+")
+        playerSymbol = "-"
+        else
+        playerSymbol = "+"
+        this.innerHTML = playerSymbol;
+        this.classList.add(playerSymbol.toLowerCase());
+                    
+                }
+            }
+        );
+    }
+
+
+
+    let winPos = [
+        [1, 2, 3], [4, 5, 6], 
+        [7, 8, 9], [1, 4, 7], 
+        [2, 5, 8], [3, 6, 9], 
+        [1, 5, 9], [3, 5, 7]
+        ]; 
+
+        
+
+        function checkWin() {
+            for (let i = 0; i < winPos.length; i++) {
+                if (
+            document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
+            document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
+            document.getElementById(winPos[i][2]).innerHTML === playerSymbol
+                  
+        ) {
+            document.getElementById(winPos[i][0]).classList.add("win");
+            document.getElementById(winPos[i][1]).classList.add("win");
+            document.getElementById(winPos[i][2]).classList.add("win"); 
+            gameEnded = true;
+                        
+                    setTimeout(function() {
+                    alert(playerSymbol + " wins!");
+                    }, 500); 
+                } 
+            } 
+        }
+
+            document.getElementById("reset").addEventListener(
+            "click", 
+            function() {
+            for (let i = 1; i <= 9; i++) {
+            document.getElementById(i.toString()).innerHTML = "";
+            document.getElementById(i.toString()).classList.remove("+");
+            document.getElementById(i.toString()).classList.remove("-");
+            document.getElementById(i.toString()).classList.remove("win");
+            gameEnded = false;
+        }
+    }
+); 
+            
+
+        
